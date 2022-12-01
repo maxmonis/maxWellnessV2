20df5be825258a9ec2b5607ee876ee32fd91a41c
Max Monis <mmonis77@gmail.com>
2022-12-01 12:29:57 -0500

    chore(MUI) install and configure MUI, add dark mode

--- src/App.tsx
+++ src/App.tsx
@@ -1,3 +1,28 @@
+import {
+  createTheme,
+  responsiveFontSizes,
+  ThemeProvider,
+} from "@mui/material/styles"
+import CssBaseline from "@mui/material/CssBaseline"
+import Typography from "@mui/material/Typography"
+import useMediaQuery from "@mui/material/useMediaQuery"
+
 export default function App() {
-  return <h1>maxWellness</h1>
+  const prefersDark = useMediaQuery("(prefers-color-scheme: dark)")
+
+  const theme = responsiveFontSizes(
+    createTheme({
+      palette: {
+        mode: prefersDark ? "dark" : "light",
+      },
+      spacing: (factor = 1) => `${0.25 * factor}rem`,
+    }),
+  )
+
+  return (
+    <ThemeProvider theme={theme}>
+      <CssBaseline enableColorScheme />
+      <Typography variant="h1">maxWellness</Typography>
+    </ThemeProvider>
+  )
 }
