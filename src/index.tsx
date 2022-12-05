932f57d61f507d19c1c510636c320e0bcf4fa12b
Max Monis <mmonis77@gmail.com>
2022-12-05 17:40:25 -0500

    test(MSW): install and set up MSW

--- src/index.tsx
+++ src/index.tsx
@@ -1,8 +1,13 @@
 import { StrictMode } from "react"
 import { createRoot } from "react-dom/client"
 
+import { worker } from "./mocks/worker"
 import App from "./App"
 import reportWebVitals from "./reportWebVitals"
+
+if (process.env.REACT_APP_USE_MSW) {
+  worker.start()
+}
 
 createRoot(document.getElementById("root") as HTMLElement).render(
   <StrictMode>
